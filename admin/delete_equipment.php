7ac5a29cd757c53042b23fde1cd9efae195aef1d
abielbalbuena43 <159741866+abielbalbuena43@users.noreply.github.com>
2025-01-23 10:52:21 +0800

    Update delete_equipment.php

--- admin/delete_equipment.php
+++ admin/delete_equipment.php
@@ -1,14 +1,12 @@
 <?php
 include "../user/connection.php";
 
-// Get the  ID from the URL
-$id = $_GET["id"];
+// Get the Equipment ID from the URL
+$equipment_id = $_GET["equipment_id"];
 
-
-mysqli_query($link, "DELETE FROM new_equipment WHERE id = $id");
+mysqli_query($link, "DELETE FROM new_equipment WHERE equipment_id = $equipment_id");
 ?>
 
 <script type="text/javascript">
-    
     window.location = "add_new_equipment.php";
 </script>
