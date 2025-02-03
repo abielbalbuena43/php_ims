53c7df674dd553db6ef5c30c072e3e1e9ac0b1fe
abielbalbuena43 <159741866+abielbalbuena43@users.noreply.github.com>
2025-02-03 11:40:26 +0800

    Update search_otherdevices.php

--- admin/search_otherdevices.php
+++ admin/search_otherdevices.php
@@ -1,38 +1,46 @@
 <?php
 include "../user/connection.php";
 
+// Check if the search query is set
 if (isset($_POST['query'])) {
+    // Escape the search query to prevent SQL injection
     $search = mysqli_real_escape_string($link, $_POST['query']);
 
-    // Search query: Matches closest results using LIKE
+    // Query to search in the fields of the 'otherdevices' table
     $query = "SELECT * FROM otherdevices 
-              WHERE od_name LIKE '%$search%' 
-                 OR od_pcname LIKE '%$search%' 
-                 OR od_assettag LIKE '%$search%' 
-                 OR od_brand LIKE '%$search%' 
-                 OR od_modelnumber LIKE '%$search%' 
-                 OR od_macaddress LIKE '%$search%'";
+              WHERE device_type LIKE '%$search%' 
+                 OR device_name LIKE '%$search%' 
+                 OR device_assettag LIKE '%$search%' 
+                 OR device_brand LIKE '%$search%' 
+                 OR device_modelnumber LIKE '%$search%' 
+                 OR device_deviceage LIKE '%$search%' 
+                 OR device_pcname LIKE '%$search%' 
+                 OR device_macaddress LIKE '%$search%'";
 
+    // Execute the query
     $res = mysqli_query($link, $query);
     
+    // Check if any records match the search query
     if (mysqli_num_rows($res) > 0) {
+        // Loop through each result and display it in the table format
         while ($row = mysqli_fetch_array($res)) {
             echo "<tr>
-                    <td>{$row["od_name"]}</td>
-                    <td>{$row["od_pcname"]}</td>
-                    <td>{$row["od_assettag"]}</td>
-                    <td>{$row["od_brand"]}</td>
-                    <td>{$row["od_modelnumber"]}</td>
-                    <td>{$row["od_deviceage"]}</td>
-                    <td>{$row["od_macaddress"]}</td>
-                    <td>{$row["od_dateacquired"]}</td>
-                    <td>" . ($row["od_dateedited"] ? $row["od_dateedited"] : 'N/A') . "</td>
-                    <td>{$row["od_remarks"]}</td>
-                    <td><a href='edit_otherdevices.php?od_id={$row['od_id']}' class='btn btn-primary'>Edit</a></td>
-                    <td><a href='delete_otherdevices.php?od_id={$row['od_id']}' class='btn btn-danger' onclick='return confirm(\"Are you sure?\")'>Delete</a></td>
+                    <td>{$row['device_type']}</td>
+                    <td>{$row['device_name']}</td>
+                    <td>{$row['device_assettag']}</td>
+                    <td>{$row['device_brand']}</td>
+                    <td>{$row['device_modelnumber']}</td>
+                    <td>{$row['device_deviceage']}</td>
+                    <td>{$row['device_pcname']}</td>
+                    <td>{$row['device_macaddress']}</td>
+                    <td>{$row['device_dateacquired']}</td>
+                    <td>" . ($row['device_dateedited'] ? $row['device_dateedited'] : 'N/A') . "</td>
+                    <td><a href='edit_otherdevices.php?od_id={$row['device_id']}' class='btn btn-primary'>Edit</a></td>
+                    <td><a href='delete_otherdevices.php?od_id={$row['device_id']}' class='btn btn-danger' onclick='return confirm(\"Are you sure you want to delete this device?\")'>Delete</a></td>
                   </tr>";
         }
     } else {
+        // If no records are found, show a message
         echo "<tr><td colspan='12' class='text-center'><strong>No matching devices found.</strong></td></tr>";
     }
 }
